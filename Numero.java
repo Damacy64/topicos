1b38fcf8397179fe3af062042f5c49c3ac150ec2
Damacy64 <pzx6464@gmail.com>
2022-03-22 19:45:49 -0600

    first commit

---
+++ Numero.java
@@ -0,0 +1,103 @@
+package topicos;
+
+/**
+ *
+ * @author pzx64
+ */
+public class Numero {
+
+    int n;
+
+    Numero(int n) {
+        this.n = n;
+    }
+
+    int mide() {
+        int copian = n, c = 0;
+
+        for (; copian > 0; copian /= 10) {
+            c++;
+        }
+
+        return c;
+    }
+
+    int contar(int ncuenta) {
+        int copian = n, c = 0;
+        do {
+            if (copian % 10 == ncuenta) {
+                c++;
+            }
+
+            copian /= 10;
+        } while (copian > 0);
+
+        return c;
+    }
+
+    int voltea() {
+        int r = 0, m = mide() - 1, copian = n;
+
+        for (; copian > 0; copian /= 10, m--) {
+            r += (copian % 10) * Math.pow(10, m);
+        }
+        return r;
+    }
+
+    int cambia(int a, int b) {
+        int r = 0, m = 0, copian = n;
+
+        while (copian > 0) {
+            if (copian % 10 == a) {
+                r += b * Math.pow(10, m);
+                m++;
+            } else {
+                r += (copian % 10) * Math.pow(10, m);
+                m++;
+            }
+            copian/=10;
+        }
+        return r;
+    }
+
+    int envuelve(int a, int b) {
+        int copian = n, r = 0, m = 0;
+
+        while (copian > 0) {
+
+            if (copian % 10 == a) {
+
+                r += b * Math.pow(10, m);
+
+                m++;
+
+                r += (copian % 10) * Math.pow(10, m);
+
+                m++;
+
+                r += b * Math.pow(10, m);
+
+                m++;
+
+                copian /= 10;
+
+            } else {
+                r += (copian % 10) * Math.pow(10, m);
+                m++;
+                copian /= 10;
+
+            }
+        }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        Numero numero = new Numero(154935);
+        System.out.println("El numero es: " + numero.n);
+        System.out.println("Mide: " + numero.mide());
+        System.out.println("Contar: " + numero.contar(5));
+        System.out.println("Voltear: " + numero.voltea());
+        System.out.println("Cambiar: " + numero.cambia(9, 3));
+        System.out.println("Envolver: " + numero.envuelve(5, 1));
+    }
+}
